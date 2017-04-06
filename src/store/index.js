97ee24d10424d13ee15ee30597f4fe41770a1388
Nigel O'Toole <pure@purestructure.com>
2017-04-06 11:19:52 +0100

    Version 1 of multiple API calls

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
 
+const apiURL = 'https://gateway.marvel.com:443/v1/public/';
+const apiKey = '&apikey=26141e57d1bf781685637a4259a48882';
+
+const apiCalls = ['characters?series=2265&limit=100', 'characters?series=2265&limit=100&offset=100'];
+
 export default new Vuex.Store({
   state: {
     characters: [],
@@ -17,14 +22,40 @@ export default new Vuex.Store({
     // X-Men (1991 - 2001); series id = 2265
     loadCharacters: ({ commit }) => {
 
-      fetch('https://gateway.marvel.com:443/v1/public/characters?series=2265&orderBy=-modified&limit=10&apikey=26141e57d1bf781685637a4259a48882')
+      // Version 1 - Muliple API calls
+      // const apiCall1 = 
+      //   fetch(`${apiURL}${apiCalls[0]}${apiKey}`)
+      //   .then(response => response.json())
+      //   .then(response => {
+      //     if (typeof response.data === 'string') {
+      //       response.data = JSON.parse(response.data);
+      //     }
+      //     return response.data.results;
+      // });
+
+      // const apiCall2 = 
+      //   fetch(`${apiURL}${apiCalls[1]}${apiKey}`)
+      //   .then(response => response.json())
+      //   .then(response => {
+      //     if (typeof response.data === 'string') {
+      //       response.data = JSON.parse(response.data);
+      //     }
+      //     return response.data.results;
+      // });
+
+      // Promise.all([apiCall1, apiCall2]).then(function(values){
+      //   const combinedData = values[0].concat(values[1]);
+      //   return combinedData;
+      // });
+
+      fetch(`${apiURL}${apiCalls[0]}${apiKey}`)
         .then(resp => resp.json())
         .then(resp => {
 
           if (typeof resp.data === 'string') {
             resp.data = JSON.parse(resp.data);
           }
-
+          // console.log(resp.data.results);
           commit('addCharacters', resp.data.results);
             
         })
