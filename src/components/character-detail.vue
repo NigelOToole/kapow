5af8ae00b4f5433b572fe5ac2153b6b34303b542
Nigel O'Toole <pure@purestructure.com>
2017-05-17 15:55:20 +0100

    Update styling to BEM standards and change router to history mode

--- src/components/character-detail.vue
+++ src/components/character-detail.vue
@@ -3,13 +3,13 @@
   <div class="container">
     <router-link to="/" class="back">&lt; Back to list</router-link>
 
-    <div class="char-detail">
+    <div class="characters characters--detail">
 
-      <div class="char-detail-item">
-        <img :src="`${ characterDetail.thumbnail.path }.${ characterDetail.thumbnail.extension }`" class="img-fluid char-detail-item-img">
+      <div class="characters--detail__item">
+        <img :src="`${ characterDetail.thumbnail.path }.${ characterDetail.thumbnail.extension }`" class="img-fluid characters--detail__img">
       </div>
 
-      <div class="char-detail-item">
+      <div class="characters--detail__item">
         <h1>{{ characterDetail.name }}</h1>
         <p>{{ characterDetail.description }}</p>
         <a :href="`${ characterDetail.moreInfoURL }`" target="_blank" class="btn">More information</a>
@@ -41,25 +41,3 @@
   };
 
 </script>
-
-
-<style lang="scss">
-  @import "../styles/variables";
-
-  // Display the details in a 2x grid
-  .char-detail {
-    display: grid;
-    grid-gap: 20px;
-    margin: 20px 0;
-
-    @media (min-width: $screen-sm-min) {
-      grid-template-columns: repeat(2, 1fr); 
-    }
-    
-  }
-
-      .char-detail-item-img {
-        border: 5px solid #333;
-      }
-
-</style>
--- src/components/character-list.vue
+++ src/components/character-list.vue
@@ -1,11 +1,11 @@
 <template>
 
   <div class="container">
-    <div class="char-list">
+    <div class="characters characters--list">
 
-      <router-link v-for="item in charactersList" :key="item.id" :to="{ name: 'characterDetail', params: { id: item.id }}" class="char-list-item">
-        <h2 class="char-list-item-title">{{ item.name }}</h2>
-        <img :src="`${item.thumbnail.path }.${item.thumbnail.extension }`" class="img-fluid char-list-item-img">
+      <router-link v-for="item in charactersList" :key="item.id" :to="{ name: 'characterDetail', params: { id: item.id }}" class="characters--list__item">
+        <h2 class="characters--list__title">{{ item.name }}</h2>
+        <img :src="`${item.thumbnail.path }.${item.thumbnail.extension }`" class="img-fluid characters--list__img">
       </router-link>
 
     </div>
@@ -40,54 +40,3 @@
   };
 
 </script>
-
-<style lang="scss">
-  @import "../styles/variables";
-
-  // Display the characters in a grid
-  .char-list {
-    display: grid;
-    grid-gap: 20px;
-    margin: 20px 0;
-
-    @media (min-width: $screen-xs-min) {
-      // Split the grid into 4 columns, equally spaced as 1 fraction unit each. The same as '1fr 1fr 1fr 1fr'
-      grid-template-columns: repeat(2, 1fr); 
-      
-      // First two items span 2 columns and 2 rows making it twice as large
-      :nth-child(1), :nth-child(2) {
-        grid-column: span 2; 
-        grid-row: span 2;
-      }
-    }
-
-    @media (min-width: $screen-sm-min) {
-      // Split the grid into 4 columns, equally spaced as 1 fraction unit each. The same as '1fr 1fr 1fr 1fr'
-      grid-template-columns: repeat(4, 1fr); 
-    }
-
-  }
-
-    .char-list-item {
-      display: block;
-      position: relative;
-      min-height: 200px;
-      border: 5px solid #333;
-      transition: color .2s,  border-color .2s;
-
-      &:hover {
-        border-color: #000;
-      }
-    }
-
-      // Overlay the title on the image
-      .char-list-item-title {
-        position: absolute;
-        margin: 5px 10px;
-        text-shadow: $title-shadow;
-        word-break: break-word;
-      }
-      .char-list-item-img {
-      }
-
-</style>
